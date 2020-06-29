acd04d513530097c010dfdbdcf17483404d590db
timibadass <omoyenitimi@gmail.com>
2020-06-29 23:01:04 +0100

    set up dayjs for app usage

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+var dayjs = require('dayjs')
 
 Vue.config.productionTip = false
+Vue.prototype.$dayjs = dayjs
+
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
