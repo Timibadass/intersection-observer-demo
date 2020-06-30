77fbb4e8bd88d245e22047ce7a4c39a2430bc663
timibadass <omoyenitimi@gmail.com>
2020-07-01 00:27:37 +0100

    add infinite scroll to app.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from './plugins/axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {
 
-  },
-  mutations: {
+    },
+    mutations: {
 
-  },
-  actions: {
-
-  }
+    },
+    actions: {
+        async getArticles(context, page) {
+            let res = await axios({
+                url: `/everything?q=general&page=${page}`,
+                method: 'GET'
+            })
+            return res;
+        }
+    }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<h1>Infinite scroll</h1>
+		<div class="articles__div" v-if="articles">
+			<news-card
+				class="news__component"
+				v-for="(article, index) in articles"
+				:key="index"
+				:article="article"
+			></news-card>
+		</div>
+		<div v-else class="news--empty">
+			<p>Loading...</p>
+		</div>
+		<div ref="articlesContainer" class="news--empty">
+			<p>Loading...</p>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	import { mapActions } from "vuex";
+	import NewsCard from "../components/newsCard";
+	const options = {
+		root: document.querySelector(".home"),
+		rootMargin: "0px",
+		threshold: 0.5,
+	};
+	// @ is an alias to /src
+	export default {
+		name: "home",
+		components: {
+			NewsCard,
+		},
+		data() {
+			return {
+				articles: [],
+				page: 1,
+				observer: null,
+			};
+		},
+		mounted() {
+			this.fetchArticles();
+		},
+		watch: {
+			articles: "setObserver",
+		},
+		methods: {
+			...mapActions(["getArticles"]),
+			callback: function (entries, observer) {
+				entries.forEach((entry) => {
+					if (entry.intersectionRatio > 0) {
+						if (
+							this.articles.length !== 100 &&
+							this.articles.length / 20 === this.page
+						) {
+							this.page += 1;
+							this.fetchArticles();
+						}
+					}
+				});
+			},
+			setObserver() {
+				this.observer = new IntersectionObserver(this.callback, options);
+				this.observer.observe(this.$refs.articlesContainer);
+			},
+			async fetchArticles() {
+				let page = this.page;
+				try {
+					let { data } = await this.getArticles(page);
+					let articles = this.articles.slice();
+					this.articles = articles.concat(data.articles);
+				} catch (error) {
+					console.error(error);
+				}
+			},
+		},
+	};
 </script>
