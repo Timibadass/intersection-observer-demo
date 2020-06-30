db421dc19a19c02fdb0babba3506997015be0f4f
timibadass <omoyenitimi@gmail.com>
2020-06-30 23:09:37 +0100

    clean up app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<router-view />
+	</div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+	@import url("https://fonts.googleapis.com/css2?family=Abel&family=Lobster&family=Staatliches&display=swap");
+	html,
+	#app {
+		min-height: 100vh;
+	}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+	#app {
+		font-family: "Abel", sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		background-color: #fff;
+	}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+	#app h1 {
+		font-family: "Staatliches", cursive;
+	}
+
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	a.router-link-exact-active {
+		color: #42b983;
+	}
+
+	.articles__div {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	@keyframes blink {
+		from {
+			opacity: 1;
+		}
+		to {
+			opacity: 0;
+		}
+	}
+	.news--empty {
+		height: 250px;
+		margin-top: 30px;
+		animation: blink 0.8s ease-in-out infinite alternate both;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-family: "Lobster", cursive;
+	}
 </style>
